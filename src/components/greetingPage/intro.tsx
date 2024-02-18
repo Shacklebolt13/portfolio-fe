a0b102aa9fac54ecd797d32d0d57b1f4dfa27210
Shacklebolt13 <grimmshax@gmail.com>
2024-02-18 21:40:57 +0530

    added space

--- src/components/greetingPage/intro.tsx
+++ src/components/greetingPage/intro.tsx
@@ -7,7 +7,7 @@ export default function Intro() {
       <div className="flex flex-grow justify-center items-center ">
         <div className="flex flex-col justify-center items-center">
           <h1 className="text-4xl font-bold text-center">
-            Hi, I'm
+            Hi, I'm{" "}
             <span className="text-primary">Gagandeep Singh Bhambrah</span>
           </h1>
           <p className="text-2xl text-center">I'm a full-stack developer</p>
