e505f66353a8d9ac484a5410ffebc9970fc7bc34
Shacklebolt13 <grimmshax@gmail.com>
2024-02-18 21:44:25 +0530

    Update intro text in greetingPage component

--- src/components/greetingPage/intro.tsx
+++ src/components/greetingPage/intro.tsx
@@ -7,10 +7,10 @@ export default function Intro() {
       <div className="flex flex-grow justify-center items-center ">
         <div className="flex flex-col justify-center items-center">
           <h1 className="text-4xl font-bold text-center">
-            Hi, I'm{" "}
+            Hi, I am{" "}
             <span className="text-primary">Gagandeep Singh Bhambrah</span>
           </h1>
-          <p className="text-2xl text-center">I'm a full-stack developer</p>
+          <p className="text-2xl text-center">I am a full-stack developer</p>
         </div>
       </div>
     </>
