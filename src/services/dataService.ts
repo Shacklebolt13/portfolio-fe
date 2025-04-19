bd85a99cc99d832c6af37f69c8bdd8de7c100a92
gagandeep_13 <gagandeep.bhambrah@calsoftinc.com>
2025-04-19 23:47:21 +0530

    feat: :sparkles: Refactor dataService to modularize education, experience, projects, and skills retrieval functions

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -30,117 +30,132 @@ export default function getContact() {
   } as ContactModel;
 }
 
+function getEducation() {
+  return [
+    {
+      icon: "/icons/giet.png",
+      title: "B.Tech in CSE",
+      id: "2019-B.TECH-GIETU",
+      description: "Graduated from GIET University, Gunupur with a CGPA of 8.2",
+    },
+    {
+      icon: "/icons/dems.png",
+      title: "Higher Secondary Education",
+      id: "2017-HSC-DEMS",
+      description: "Completed my HSC from Deepika English Medium School with a percentage of 75.6%",
+    }
+  ]
+}
+
+function getExperience() {
+  return [
+    {
+      id: "2021-08-RethinkUX",
+      title: "SDE intern @ Rethink UX",
+      icon: "/icons/rethinkux.png",
+      description: "Interned as an SDE Intern at Rethink UX"
+    },
+    {
+      description: "Freelanced for a project at Brane Enterprises",
+      icon: "/icons/brane.png",
+      id: "2022-03-Brane",
+      title: "Consultant @ Brane Enterprises"
+    },
+    {
+      title: "Co-Founded @ Wisbox",
+      icon: "/icons/wisbox.png",
+      id: "2022-04-Wisbox",
+      description: "Co-Founded Wisbox and worked as a CTO."
+    },
+    {
+      description: "Interned as an SDE Intern at Calsoft Inc.",
+      title: "SDE Intern @ Calsoft Inc.",
+      id: "2022-07-Calsoft",
+      icon: "/icons/calsoft.png"
+    },
+    {
+      icon: "/icons/jatrajagat.webp",
+      description: "Consulted for JatraJagat.",
+      title: "Consultant @ JatraJagat",
+      id: "2022-10-JatraJagat"
+    },
+    {
+      description: "Contributed as a Development Engineer at Calsoft Inc.",
+      id: "2023-07-Calsoft",
+      icon: "/icons/calsoft.png",
+      title: "Development Engineer @ Calsoft Inc."
+    },
+    {
+      title: "Senior Development Engineer @ Calsoft Inc.",
+      id: "2024-08-Calsoft",
+      icon: "/icons/calsoft.png",
+      description: "Contributed as a Development Engineer at Calsoft Inc."
+    }
+  ]
+}
+
+function getProjects() {
+  return [
+    {
+      "icon": "/icons/pentest.png",
+      "title": "Captive Portal Pentest",
+      "id": "2019-12-CaptivePortalPentest",
+      "description": "Brute forced college captive portal using python and android."
+    }, {
+      "id": "2020-04-CollegeDataScraper",
+      "title": "GIETU Attendance Scraper",
+      "description": "Created a attendance scraper to fetch attendance, marks, profile etc. from our college website.",
+      "icon": "/icons/giet.png"
+    }, {
+      "title": "Fame Now : Social Media",
+      "description": "Created a Social Media platform under training by Era Interfaces",
+      "icon": "/icons/famenow.png",
+      "id": "2021-04-FameNow"
+    }, {
+      "title": "GIET All In One Application",
+      "description": "Created a backend for our College's All in One Application.",
+      "id": "2021-08-CollegeAIOApp",
+      "icon": "/icons/giet.png"
+    }, {
+      "title": "Smart Home Application Backend",
+      "description": "Built a real time backend for a smart home application",
+      "id": "2022-03-SmartHomeAppBackend",
+      "icon": "/icons/smarthome.avif"
+    }, {
+      "icon": "/icons/ecommerce.svg",
+      "description": "Built an E-Commerce Platform for Wisbox",
+      "id": "2022-05-ECommercePlatform",
+      "title": "E-Commerce Platform"
+    }
+  ]
+}
+
+function getSkills() {
+  return [
+    {
+      "icon": "/icons/golang.svg",
+      "id": "golang",
+      "description": "My Experience with Go Lang",
+      "title": "Go-Lang"
+    }, {
+      "title": "Java",
+      "id": "java",
+      "description": "My Experience with the Java Language",
+      "icon": "/icons/java.svg"
+    }, {
+      "description": "My Experience with the Python Language",
+      "icon": "/icons/python.svg",
+      "title": "Python",
+      "id": "python"
+    }
+  ]
+}
+
 export function getDisplayData(): Map<string, TabModel[]> {
-  const data = {
-    [EDUCATION_SUBSECTION]: [
-      {
-        icon: "/icons/giet.png",
-        title: "B.Tech in CSE",
-        id: "2019-B.TECH-GIETU",
-        description: "Graduated from GIET University, Gunupur with a CGPA of 8.2",
-      },
-      {
-        icon: "/icons/dems.png",
-        title: "Higher Secondary Education",
-        id: "2017-HSC-DEMS",
-        description: "Completed my HSC from Deepika English Medium School with a percentage of 75.6%",
-      }
-    ],
-    [EXPERIENCE_SUBSECTION]: [
-      {
-        id: "2021-08-RethinkUX",
-        title: "SDE intern @ Rethink UX",
-        icon: "/icons/rethinkux.png",
-        description: "Interned as an SDE Intern at Rethink UX"
-      },
-      {
-        description: "Freelanced for a project at Brane Enterprises",
-        icon: "/icons/brane.png",
-        id: "2022-03-Brane",
-        title: "Consultant @ Brane Enterprises"
-      },
-      {
-        title: "Co-Founded @ Wisbox",
-        icon: "/icons/wisbox.png",
-        id: "2022-04-Wisbox",
-        description: "Co-Founded Wisbox and worked as a CTO."
-      },
-      {
-        description: "Interned as an SDE Intern at Calsoft Inc.",
-        title: "SDE Intern @ Calsoft Inc.",
-        id: "2022-07-Calsoft",
-        icon: "/icons/calsoft.png"
-      },
-      {
-        icon: "/icons/jatrajagat.webp",
-        description: "Consulted for JatraJagat.",
-        title: "Consultant @ JatraJagat",
-        id: "2022-10-JatraJagat"
-      },
-      {
-        description: "Contributed as a Development Engineer at Calsoft Inc.",
-        id: "2023-07-Calsoft",
-        icon: "/icons/calsoft.png",
-        title: "Development Engineer @ Calsoft Inc."
-      },
-      {
-        title: "Senior Development Engineer @ Calsoft Inc.",
-        id: "2024-08-Calsoft",
-        icon: "/icons/calsoft.png",
-        description: "Contributed as a Development Engineer at Calsoft Inc."
-      }
-    ],
-    [PROJECT_SUBSECTION]: [
-      {
-        "icon": "/icons/pentest.png",
-        "title": "Captive Portal Pentest",
-        "id": "2019-12-CaptivePortalPentest",
-        "description": "Brute forced college captive portal using python and android."
-      }, {
-        "id": "2020-04-CollegeDataScraper",
-        "title": "GIETU Attendance Scraper",
-        "description": "Created a attendance scraper to fetch attendance, marks, profile etc. from our college website.",
-        "icon": "/icons/giet.png"
-      }, {
-        "title": "Fame Now : Social Media",
-        "description": "Created a Social Media platform under training by Era Interfaces",
-        "icon": "/icons/famenow.png",
-        "id": "2021-04-FameNow"
-      }, {
-        "title": "GIET All In One Application",
-        "description": "Created a backend for our College's All in One Application.",
-        "id": "2021-08-CollegeAIOApp",
-        "icon": "/icons/giet.png"
-      }, {
-        "title": "Smart Home Application Backend",
-        "description": "Built a real time backend for a smart home application",
-        "id": "2022-03-SmartHomeAppBackend",
-        "icon": "/icons/smarthome.avif"
-      }, {
-        "icon": "/icons/ecommerce.svg",
-        "description": "Built an E-Commerce Platform for Wisbox",
-        "id": "2022-05-ECommercePlatform",
-        "title": "E-Commerce Platform"
-      }
-    ],
-    [SKILL_SUBSECTION]: [
-      {
-        "icon": "/icons/golang.svg",
-        "id": "golang",
-        "description": "My Experience with Go Lang",
-        "title": "Go-Lang"
-      }, {
-        "title": "Java",
-        "id": "java",
-        "description": "My Experience with the Java Language",
-        "icon": "/icons/java.svg"
-      }, {
-        "description": "My Experience with the Python Language",
-        "icon": "/icons/python.svg",
-        "title": "Python",
-        "id": "python"
-      }
-    ],
-  };
-  return new Map(Object.entries(data));
+  return new Map(Object.entries({
+    [EDUCATION_SUBSECTION]: getEducation(),
+    [EXPERIENCE_SUBSECTION]: getExperience(),
+    [PROJECT_SUBSECTION]: getProjects(),
+    [SKILL_SUBSECTION]: getSkills(),
+  }));
 }
